950858b0a31697246b571521d5bd624714619280
EdgarsSmirnovs <56843400+EdgarsSmirnovs@users.noreply.github.com>
2020-03-31 01:23:08 +0300

    getPrefixFun

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "discrete-lab03"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.10"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.1.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
--- src/main/scala/lv/rbs/ds/lab03/KMPmatcher.scala
+++ src/main/scala/lv/rbs/ds/lab03/KMPmatcher.scala
@@ -1,15 +1,41 @@
 package lv.rbs.ds.lab03
 
+import scala.collection.mutable.ListBuffer
+
 class KMPmatcher(var pattern: String) {
-  def getPrefixFun(): List[Int] = {
-    return List()
+
+  var listToReturn = new ListBuffer[Int]()
+
+  def getPrefixFun: List[Int] = {
+
+    val m = pattern.length
+    var k = 0
+
+    for (i <- 1 to m) {
+      listToReturn += i
+    }
+    listToReturn(0) = -1
+    listToReturn(1) = 0
+
+    for (q <- 2 to m) {
+      while (k > 0 && this.pattern(k) != this.pattern(q-1)) {
+        k = listToReturn(k)
+      }
+      if (this.pattern(k) == this.pattern(q-1)) {
+        k += 1
+      }
+      listToReturn(q) = k
+    }
+    listToReturn.toList
   }
 
   def findAllIn(text: CharSequence): Iterator[Int] = {
-    return List().iterator
+    val newText = "DEMO"
+    listToReturn.iterator
   }
 
   def toJson(text: CharSequence): String = {
-    return ""
+    ""
   }
+
 }
