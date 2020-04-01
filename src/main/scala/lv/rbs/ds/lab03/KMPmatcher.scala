f051d1cef276570594b6c6afc42320365a800e39
EdgarsSmirnovs <56843400+EdgarsSmirnovs@users.noreply.github.com>
2020-04-01 19:19:56 +0300

    findAllIn

--- src/main/scala/lv/rbs/ds/lab03/KMPmatcher.scala
+++ src/main/scala/lv/rbs/ds/lab03/KMPmatcher.scala
@@ -4,10 +4,9 @@ import scala.collection.mutable.ListBuffer
 
 class KMPmatcher(var pattern: String) {
 
-  var listToReturn = new ListBuffer[Int]()
-
   def getPrefixFun: List[Int] = {
 
+    var listToReturn = new ListBuffer[Int]()
     val m = pattern.length
     var k = 0
 
@@ -18,10 +17,10 @@ class KMPmatcher(var pattern: String) {
     listToReturn(1) = 0
 
     for (q <- 2 to m) {
-      while (k > 0 && this.pattern(k) != this.pattern(q-1)) {
+      while (k > 0 && this.pattern(k) != this.pattern(q - 1)) {
         k = listToReturn(k)
       }
-      if (this.pattern(k) == this.pattern(q-1)) {
+      if (this.pattern(k) == this.pattern(q - 1)) {
         k += 1
       }
       listToReturn(q) = k
@@ -30,12 +29,39 @@ class KMPmatcher(var pattern: String) {
   }
 
   def findAllIn(text: CharSequence): Iterator[Int] = {
-    val newText = "DEMO"
-    listToReturn.iterator
-  }
 
-  def toJson(text: CharSequence): String = {
-    ""
-  }
+    var stringSearch = new ListBuffer[Int]()
+    val textLength = text.length
+    val prefixLength = this.pattern.length
+    val f = getPrefixFun
+    var k = 0
 
+    for (i <- 0 until textLength) {
+      while (k > 0 && this.pattern(k) != text.charAt(i)) {
+        k = f(k)
+      }
+      if (this.pattern(k) == text.charAt(i)) {
+        k += 1
+      }
+      if (k == prefixLength) {
+        var printOut = i - prefixLength + 1
+        println("Pattern appears with shift: " + printOut)
+        stringSearch += printOut
+        k = f(k)
+      }
+    }
+    stringSearch.toList.iterator
+  }
 }
+
+//  def toJson(text: CharSequence): String = {
+//    val offsets = findAllIn(text)
+//    val json: JValue = ("algorithm" -> "KMP") ~
+//      ("pattern" -> pattern) ~
+//      ("text" -> text.toString) ~
+//      ("steps" -> {
+//        for (number <- 1 until 3) {}
+//      }
+//  }
+//}
+
